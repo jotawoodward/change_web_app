f8303e04292df4455bc918f5e8467c5225adddb2
Jo Woodward <jo.woodward@experian.com>
2021-03-23 15:03:25 +0000

    bit more css

--- api/calc_change.py
+++ api/calc_change.py
@@ -10,9 +10,7 @@ def calc_change(diff):
     options = [50.0, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
     calc_list=[]
     for x in options:
-        print(diff)
         number= int(Decimal(str(diff))/Decimal(str(x)))
-        print(number)
         diff = Decimal(str(diff))- Decimal(str(number))*Decimal(str(x))
         calc_list.append({"piece":x, "number": number})
     return calc_list
@@ -31,7 +29,7 @@ def api():
     amount = validate("amount")
     price = validate("price")
 
-    calc_list = calc_change(amount - price)
+    calc_list = calc_change(Decimal(str(amount)) - Decimal(str(price)))
     return flask.jsonify( calc_list )
 
 if __name__ == "__main__":
